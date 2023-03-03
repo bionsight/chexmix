07d04ef19a902347f84a1fff6b13d8d68d2a4538
junseok <hersheypq@gmail.com>
2023-03-03 15:05:00 +0900

    test setup

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     url='https://github.com/bionsight/chexmix',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')] + 'src/data' + 'src/datasources',
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')] + 'data' + 'datasources',
     include_package_data=True,
     zip_safe=False,
     classifiers=[
